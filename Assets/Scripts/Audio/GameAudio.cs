300b627fd360b23fca953717701404786733812e
nvargas5 <nv3019@gmail.com>
2019-05-01 12:35:54 -0400

    Added Music and Opening effect
    added an explosion effect on the start of main menu scen
    
    song added on start of game

---
+++ Assets/Scripts/Audio/GameAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAudio : MonoBehaviour
+{
+    public AudioClip soundEffect;
+
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = soundEffect;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
