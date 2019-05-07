91c8be41a69b03828c6dfbb2b247dd4daf1b7c43
Solojkim <38299770+Solojkim@users.noreply.github.com>
2019-05-06 23:40:34 -0400

    Death Menu stuff
    Please work this time

---
+++ Assets/Scripts/Menus/Death.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Death : MonoBehaviour
+{
+    public GameObject deathMenu;
+    public GameAudio gameAudio;
+    public GameObject player;
+
+
+    // Update is called once per frame
+
+    public Button restartButt;
+    public Button quitButt;
+
+    void Start()
+    {
+
+
+        restartButt = restartButt.GetComponent<Button>();
+        restartButt.onClick.AddListener(Restart);
+        quitButt = quitButt.GetComponent<Button>();
+        quitButt.onClick.AddListener(QuitMenu);
+    }
+
+    void Update()
+    {
+        if (!player.GetComponent<PlayerController>().alive)
+        {
+            deathMenu.SetActive(true);
+            GetComponent<PauseMenu>().enabled = false;
+        }
+
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void QuitMenu()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
